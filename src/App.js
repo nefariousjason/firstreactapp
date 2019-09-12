22e53b89a9ad281ab8e474fd5c4a3fe0a1ea6abf
Jason Stewart <jason.stewart1124@gmail.com>
2019-09-11 20:33:37 -0500

    added first component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component{
         {
         id: 2,
         title: "Dinner with wife",
-        completed: false
+        completed: true
         },
         {
         id: 3,
@@ -23,10 +23,20 @@ class App extends Component{
         }
     ]
 }
+// Toggle complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo =>{
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
   render(){
     return (
     <div className="App">
-      <Todos todos={this.state.todos} />
+      <Todos todos={this.state.todos} markComplete={this.markComplete} />
     </div>
     )};
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+    getStyle = () => {
+        return{
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+    }
+
+
+
+    render() {
+        const { id, title }=this.props.todo;
+        return (
+            <div style={this.getStyle()}>
+                <p>
+                <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}></input> {' '}
+                { title }
+                <button style={btnStyle}>X</button> {' '}
+                </p>
+            </div>
+        )
+    }
+}
+
+// PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+
+}
+
+export default TodoItem
